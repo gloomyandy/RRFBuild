cd06f61efd2cac867e7bd04f902e2b54c6580ad6
Andy Shaw <andy-git@gloomy-place.com>
2020-09-17 17:45:22 +0100

    Updated STM core name and new filenames

--- BuildRelease.sh
+++ BuildRelease.sh
@@ -1,18 +1,19 @@
 #!/bin/sh
 echo $1 $2 $3
 BUILD=${1:-Debug}
-CORE=${2:-STM32}
+CORE=${2:-STM32F4}
 NETWORK=${3:-SBC}
+TMC22XX=${4:-true}
 #extract firmware version from header file
 VER=`awk 'sub(/.*MAIN_VERSION/,""){print $1}' RepRapFirmware/src/Version.h  | awk 'gsub(/"/, "", $1)'`
 
-OUTPUT=releases/${VER}/${CORE}/${BUILD}
+OUTPUT=releases/${VER}/${BUILD}
 
 mkdir -p ${OUTPUT}
 
 make distclean
-make -j2 firmware CORE=${CORE} NETWORK=${NETWORK} BUILD=${BUILD} OUTPUT_NAME=firmware
+make -j2 firmware CORE=${CORE} NETWORK=${NETWORK} BUILD=${BUILD} TMC22XX=${TMC22XX} OUTPUT_NAME=firmware
 if [ -f ./build/firmware.bin ]; then
-        mv ./build/firmware.bin ${OUTPUT}/firmware-${NETWORK,,}.bin
-        mv ./build/firmware.map ${OUTPUT}/firmware-${NETWORK,,}.map
+        mv ./build/firmware.bin ${OUTPUT}/firmware-${CORE,,}-${NETWORK,,}-${VER,,}.bin
+        mv ./build/firmware.map ${OUTPUT}/firmware-${CORE,,}-${NETWORK,,}-${VER,,}.map
 fi 
--- makefile
+++ makefile
@@ -1,3 +1,3 @@
-CORE ?= STM32
+CORE ?= STM32F4
 MAKE_DIR ?= Core$(CORE)/makefiles
 include $(MAKE_DIR)/makefile
