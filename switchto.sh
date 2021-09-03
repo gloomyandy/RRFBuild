37c75a3d89fa2782f02326c1cf263d1458cd1be3
Andy Shaw <andy-git@gloomy-place.com>
2021-09-03 12:04:26 +0100

    Add new command

---
+++ switchto.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+echo "RRFBuild"
+git status 
+git checkout $1
+cd CoreN2G
+echo "CoreN2G"
+git status
+git checkout $1
+cd ../RepRapFirmware
+echo "RepRapFirmware"
+git status
+git checkout $1
+cd ../RRFLibraries
+echo "RRFLibraries"
+git status
+git checkout $1
+cd ../FreeRTOS
+echo "FreeRTOS"
+git status
+git checkout $1
+cd ..
