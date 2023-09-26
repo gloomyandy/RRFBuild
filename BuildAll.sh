f6b5cff2bf799a010bf0623ad259a80cfee417ad
Andy Shaw <andy-git@gloomy-place.com>
2023-09-26 18:41:20 +0100

    Rationalise pico defines

--- BuildAll.sh
+++ BuildAll.sh
@@ -9,5 +9,5 @@
 ./BuildIAPBLRelease.sh Debug STM32 STM32H7 2
 ./BuildExpRelease.sh Debug RP2040 FLY36RRF 0 "-DUSE_PICOCAN" "-picocan"
 ./BuildExpRelease.sh Debug RP2040 FLY36RRF 1 "-DUSE_SPICAN"
-./BuildExpRelease.sh Debug RP2040 FLYSB2040v1_0 0 "-DUSE_PICOCAN" "-picocan"
-./BuildExpRelease.sh Debug RP2040 PITBv1_0 0 "-DUSE_PICOCAN" "-picocan"
+./BuildExpRelease.sh Debug RP2040 FLYSB2040V1_0 0 "-DUSE_PICOCAN" "-picocan"
+./BuildExpRelease.sh Debug RP2040 PITBV1_0 0 "-DUSE_PICOCAN" "-picocan"
