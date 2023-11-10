9fcc4bde04a09175065b11a98823487569221d79
Andy Shaw <andy-git@gloomy-place.com>
2023-11-10 17:20:02 +0000

    Added build options for H7 SBC

--- BuildAll.sh
+++ BuildAll.sh
@@ -3,9 +3,13 @@
 #./BuildRelease.sh Release STM32F4 WIFI
 ./BuildRelease.sh Debug STM32 STM32H7 STM32H743 WIFI firmware-stm32h7-wifi
 ./BuildRelease.sh Debug STM32 STM32H7 STM32H723 WIFI firmware-stm32h723-wifi
+./BuildRelease.sh Debug STM32 STM32H7 STM32H723 SBC firmware-stm32h723-sbc
+./BuildRelease.sh Debug STM32 STM32H7 STM32H743 SBC firmware-stm32h7-sbc
 ./BuildRelease.sh Debug STM32 STM32F4 "" SBC
 ./BuildRelease.sh Debug STM32 STM32F4 "" WIFI
-./BuildIAPRelease.sh Debug STM32 STM32F4 SBC
+./BuildIAPRelease.sh Debug STM32 STM32H7 STM32H723 SBC firmware-stm32h723-sbc stm32h723_iap_SBC
+./BuildIAPRelease.sh Debug STM32 STM32H7 STM32H743 SBC firmware-stm32h7-sbc stm32h7_iap_SBC
+./BuildIAPRelease.sh Debug STM32 STM32F4 "" SBC firmware-stm32f4-sbc stm32f4_iap_SBC
 ./BuildIAPBLRelease.sh Debug STM32 STM32H7 2
 ./BuildExpRelease.sh Debug RP2040 FLY36RRF 0 "-DUSE_PICOCAN" "-picocan"
 ./BuildExpRelease.sh Debug RP2040 FLY36RRF 1 "-DUSE_SPICAN"
--- BuildIAPRelease.sh
+++ BuildIAPRelease.sh
@@ -1,24 +1,26 @@
 #!/bin/sh
-echo $1 $2 $3 $4
+echo $1 $2 $3 $4 $5 $6 $7 $8
 BUILD=${1:-Debug}
 CORE=${2:-STM32}
 MCU=${3:-STM32F4}
-NETWORK=${4:-SBC}
-TMC22XX=${5:-true}
-STARTUP_DELAY=$6
+VARIANT=$4
+NETWORK=${5:-SBC}
+FWNAME=${6:-firmware-${MCU,,}-${NETWORK,,}}
+OUTNAME=${7:-${MCU,,}_iap_SBC}
+STARTUP_DELAY=$8
 #extract firmware version from header file
 VER=`awk 'sub(/.*MAIN_VERSION/,""){print $1}' RepRapFirmware/src/Version.h  | awk 'gsub(/"/, "", $1)'`
 
 OUTPUT=releases/${VER}/${BUILD}
 
 mkdir -p ${OUTPUT}
-rm -f ${OUTPUT}/${MCU,,}_iap_SBC.*
-rm -f ${OUTPUT}/firmware-${MCU,,}-${NETWORK,,}-${VER,,}.zip
+rm -f ${OUTPUT}/${OUTNAME}.*
+rm -f ${OUTPUT}/${FWNAME}-${VER,,}.zip
 
 make distclean MAKE_DIR=IAP/makefiles/${MCU}
-make -j8 CORE=${CORE} MCU=${MCU} CONFIG=IAP_SPI_LOADER MAKE_DIR=IAP/makefiles/${MCU} all
-if [ -f ./iapbuild/${MCU,,}_iap_SBC.bin ]; then
-    mv ./iapbuild/${MCU,,}_iap_SBC.bin ${OUTPUT}/${MCU,,}_iap_SBC.bin
-    mv ./iapbuild/${MCU,,}_iap_SBC.map ${OUTPUT}/${MCU,,}_iap_SBC.map
-    (cd ${OUTPUT}; /c/Windows/SysWOW64/tar.exe -a -c -f firmware-${MCU,,}-${NETWORK,,}-${VER,,}.zip firmware-${MCU,,}-${NETWORK,,}-${VER,,}.bin ${MCU,,}_iap_SBC.bin)
+make -j8 CORE=${CORE} MCU=${MCU} VARIANT=${VARIANT} CONFIG=IAP_SPI_LOADER MAKE_DIR=IAP/makefiles/${MCU} all
+if [ -f ./iapbuild/${OUTNAME}.bin ]; then
+    mv ./iapbuild/${OUTNAME}.bin ${OUTPUT}/${OUTNAME}.bin
+    mv ./iapbuild/${OUTNAME}.map ${OUTPUT}/${OUTNAME}.map
+    (cd ${OUTPUT}; /c/Windows/SysWOW64/tar.exe -a -c -f ${FWNAME}-${VER,,}.zip ${FWNAME}-${VER,,}.bin ${OUTNAME}.bin)
 fi 
