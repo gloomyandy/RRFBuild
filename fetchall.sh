6bc0ebcd2681903d3f4a303bf5cbc0c31e43b064
Andy Shaw <andy-git@gloomy-place.com>
2024-04-20 18:50:38 +0100

    Add new helper script

---
+++ fetchall.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+cd CoreN2G
+git fetch upstream
+cd ../RepRapFirmware
+git fetch upstream
+cd ../RRFLibraries
+git fetch upstream
+cd ../CANlib
+git fetch upstream
+cd ../Duet3Expansion
+git fetch upstream
+cd ../DuetWebControl
+git fetch upstream
+cd ../WifiSocketServerRTOS
+git fetch upstream
+cd ..
