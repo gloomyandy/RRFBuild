448004863696dba09323106aee6190612d6acf66
Andy Shaw <andy-git@gloomy-place.com>
2025-11-29 13:22:11 +0000

    Fix building DWC from source

--- BuildDWC.sh
+++ BuildDWC.sh
@@ -1,10 +1,6 @@
 #!/bin/sh
 if [ -f DuetWebControl-SD.zip ]; then
     echo "Using pre-built DWC"
-elif [ -f DuetAPI.xml ]; then
-    cp DuetAPI.xml DuetWebControl
-    (cd DuetWebControl; npm run build)
-    rm DuetWebControl/DuetAPI.xml
 else
-    echo "DuetAPI.xml not found"
+    (cd DuetWebControl; npm install; npm run build)
 fi
